eb0b9a5478c248a63ddd95b79207e54d21371bd8
Blazko <denisnfoliene@gmail.com>
2025-10-10 11:48:22 -0300

    Retirada dos arquivos testes. Trabalhar agora em cima do cadastro_contatos sem o overload de operador.

---
+++ Cadastros/cadastro_contatos.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+void gotoxy(int x, int y) {
+   std::cout << "\033[" << y << ";" << x << "H";
+}
+
+void clearScreen() {
+   std::cout << "\033[2J\033[1;1H";
+}
+
+struct Contatos {
+   int id;
+   std::string nome;
+   std::string email;
+   std::string telefone;
+}
+
+int main() {
+   clearScreen();
+
+   std::cout << "--- Cadastro de Novo Contato ---" << std::endl;
+   std::cout << "Nome:     " << std::endl;
+   std::cout << "Email:    " << std::endl;
+   std::cout << "Telefone: " << std::endl;
+   std::cout << "--------------------------------" << std::endl;
+
+   Contatos contato;
+
+   gotoxy(11, 2); 
+   std::getline(std::cin, contato.nome);
+
+   gotoxy(11, 3);
+   std::getline(std::cin, contato.email);
+
+   gotoxy(11, 4); 
+   std::getline(std::cin, contato.telefone);
+
+   gotoxy(1, 6);
+   std::cout << "Contato salvo com sucesso!" << std::endl;
+   std::cout << "Nome: " << contato.nome << ", Email: " << contato.email << ", Telefone: " << contato.telefone << std::endl;
+
+   return 0;
+}
